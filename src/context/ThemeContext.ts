1efa0b5b35c35a8314c6b99856a7da8f46cc49b4
Andrii Dmtryk <admitruk237@gmail.com>
2026-04-08 14:34:05 +0200

    docs: update README and fix project build issues

--- src/context/ThemeContext.ts
+++ src/context/ThemeContext.ts
@@ -9,8 +9,12 @@ export const ThemeContext = createContext<ThemeContextType | undefined>(
   undefined,
 )
 
-export const useTheme = () => {
+export const useTheme = (): ThemeContextType => {
   const context = useContext(ThemeContext)
+
+  if (context === undefined) {
+    throw new Error('useTheme must be used within a ThemeProvider')
+  }
 
   return context
 }
